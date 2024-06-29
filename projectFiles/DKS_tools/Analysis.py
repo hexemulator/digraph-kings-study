53ca1cca617797726d61dc41623a424570bfc5c2
sarcausticGH <brandonstanton@outlook.com>
2024-06-29 10:03:31 -0700

    + wrote an algorithm for determining the closed diwalks for each king vertex, this is a correction over determining the cycles for each king vertex.
    *The above was necessary because the gcd calculation of the lengths of simple cycles containing a vertex is NOT equivalent to a gcd calculation of the lengths of closed diwalks containing a vertex.*
    
    + added 'name' arg to the init function for the Digraph class. we will want to name our digraphs to keep track of them during testing.
    
    + added a few attributes to Digraph class, 'name', 'is_valid_digraph', 'max_k_val', 'min_k_val', 'interpret_as_T', 'has_emperor'; the purposes of these attributes should be self-evident
    
    + in the set_king_vals() method (changed from 'find_kings()') in the Digraph class, it now stores/determines the max k value, and min k value, when considering the values of the k kings. also sorts the king list at end of function
    
    + added 'list_D_characteristics()' method to Digraph, lists characteristics of the digraph
    
    ~ changed 'is_tournament' arg to 'interpret_as_tournament' in the init function for the Digraph class, this is for clarity.
    
    + added 'list_digraph_strong_components()' method
    
    + added 'below_upper_bound()' method to Product_Digraph class, will check if the maximal k val is below what the paper states is the upper bound, this is for identifying digraphs of interest

--- projectFiles/DKS_tools/Analysis.py
+++ projectFiles/DKS_tools/Analysis.py
@@ -9,38 +9,58 @@ class Digraph:
     '''
     Instantiated with a DiGraph object.
     '''
-    def __init__(self, D: nx.DiGraph, is_tournament:bool=False):
+    def __init__(self, D: nx.DiGraph, name: str, interpret_as_tournament:bool=False):
         '''
         :param D: DiGraph as given by networkx.DiGraph
-        :param is_tournament: whether the DiGraph should be interpreted as a tournament, default is False
+        :param interpret_as_tournament: whether the DiGraph should be interpreted as a tournament, default is False
         '''
+        # general attributes
         self.D: nx.DiGraph = D
-        self.is_T: bool = is_tournament
-        self.D_kings: list = self.find_kings()      # gets list of king vertices
+        self.name: str = name
+        self.is_valid_digraph: bool = self.D.order() != 0
+
+        # attributes specific to kings
+        self.D_kings: list = []
+        self.max_k_val = 0; self.min_k_val = 0  # min/max start at 0 until reassigned
+        self.set_king_vals()
+
 
         if self.D_kings != []:
             self.set_Sv()
 
-    def find_kings(self) -> list:
+        # attributes specific to tournaments
+        self.is_T: bool = nx.is_tournament(self.D) and self.is_valid_digraph
+        self.interpret_as_T = interpret_as_tournament
+        self.has_emperor = True if (len(self.D_kings) and self.interpret_as_T) == 1 else False
+
+    def set_king_vals(self):
         '''
-        identifies 'king' nodes in the DiGraph, and gives each node a 'k_val' equal to the node's eccentricity
-        :param is_T: if DiGraph has been identified as a tournament, then king vertices in tournaments need k_val <= 2
-        :return: list of kings in DiGraph
+        does a number of things to do with king vertices
+        - finds vertices that are kings
+        - assigns a k_val to king vertices
+        - identifies the min/max k_vals in the digraph
+        - assigns the flat identifier list of kings to class attribute 'self.D_kings'
         '''
 
         king_list = list()
+        k_val_list = []
 
         # for each node in the digraph, attempt to get eccentricity
         for node in self.D.nodes:
             try:
                 k_val = nx.eccentricity(self.D, node)
                 self.D.nodes[node]['k_val'] = k_val                       # if successful, store K-value in node itself
+                k_val_list.append(k_val)
                 king_list.append(node)
 
             except:
                 pass
 
-        return king_list
+        if len(k_val_list) != 0:
+            self.max_k_val = max(k_val_list)
+            self.min_k_val = min(k_val_list)
+
+        self.D_kings = sorted(king_list)
 
     def set_Sv(self):
         '''
@@ -54,7 +74,7 @@ class Digraph:
             self.D.nodes[king]['Sv'] = set()
 
         # grab all cycles in the digraph D
-        all_simple_cycles = nx.simple_cycles(self.D, self.D.order())
+        all_simple_cycles = nx.simple_cycles(self.D)
 
         # looks at each node in cycles individually, if node is king, it adds len of cycle to Sv set of node
         for cycle in all_simple_cycles:
@@ -72,23 +92,78 @@ class Digraph:
             else:                                               # otherwise, we can calculate its GCD(Sv)
                 self.D.nodes[king]['GCD(Sv)'] = ft.reduce(m.gcd, self.D.nodes[king]['Sv'])
 
-    def list_kings(self, force_tournament_rules: bool=True):
+    def get_king_list(self, force_tournament_rules: bool=False) -> list:
         '''
         Lists kings in the DiGraph
         :param force_tournament_rules: if Digraph is a tournament, if set to False will ignore k_val <= 2 rule, if Digraph is not a tournament, this setting does nothing
+        :return: list comprising of kings, is only a flat list with no characteristics
         '''
         if len(self.D_kings) == 0:
-            print('No kings in DiGraph.')
-        elif self.is_T and force_tournament_rules:
+            print(f"{self.name} has no kings, nothing to list.")
+        elif self.interpret_as_T and self.is_T and force_tournament_rules:
             t_king_list = list()
 
             for king in self.D_kings:
                 if self.D.nodes[king]['k_val'] <= 2:
                     t_king_list.append(king)
 
-            print(f"Kings in Tournament are: {t_king_list}")
+            return sorted(t_king_list)
         else:
-            print(f"Kings in Digraph are: {self.D_kings}")
+            return self.D_kings
+
+    def list_D_characteristics(self, force_tournament_rules: bool=False):
+        '''
+        lists characteristics of the digraph: order, size, min/max k_val, etc.
+        :param force_tournament_rules: force listed characteristics to abide by tournament rules, if digraph is not a
+        tournament, it should have no effect if unchanged
+        '''
+        print(f"~~~~~~CHARACTERISTICS OF {self.name}~~~~~~")
+        print(f"Listing characteristics of {"tournament" if self.interpret_as_T else "digraph"}, {self.name}:")
+        print(f"Has order: {self.D.order()}, size: {self.D.size()}.")
+
+        # list kings, if they exist-- if tournament, give (n,k)-tournament spec
+        if self.is_T and force_tournament_rules:
+            print(f"Is a ({self.D.order()},{len(self.D_kings)}) tournament.")
+            if self.has_emperor:
+                print(f"Emperor vertex is: {self.D_kings[0]}.")
+            else:
+                print(f"Kings in tournament, {self.name} are: {self.get_king_list(force_tournament_rules)}.")
+        elif len(self.D_kings) != 0:
+            print(f"{self.name} has king(s), king vertices are: {self.get_king_list()}")
+            print(f"{self.name} has min_k_val: {self.min_k_val}, and max_k_val: {self.max_k_val}")
+        else:
+            print(f"{self.name} has no kings, thusly, cannot list them, and also no min/max k_val.")
+        print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
+        
+        self.list_digraph_strong_components(force_tournament_rules)
+
+    def list_king_characteristics(self, force_tournament_rules: bool=False):
+        print(f"~KING CHARACTERISTICS IN {self.name},FORCE-T-RULES={force_tournament_rules}~")
+        for node in self.D_kings:
+            if force_tournament_rules and self.D.nodes[node]['k_val'] > 2:
+                continue
+            else:
+                print(f"vertex {node} is a {self.D.nodes[node]['k_val']}-king, with Sv = {self.D.nodes[node]['Sv']}, GCD(Sv) = {self.D.nodes[node]['GCD(Sv)']}")
+        print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+    def list_digraph_strong_components(self, exclude_isolated_vertices: bool=False):
+        strong_components = nx.strongly_connected_components(self.D)
+        sorted_strong_components = sorted(strong_components, key=len, reverse=True)
+        strong_components_count = len(sorted_strong_components)
+
+        print(f"~~~~~~STRONG COMPONENTS LISTINGS IN {self.name}~~~~~~")
+
+        if strong_components_count == 1:
+            print(f"{self.name} has singleton strong component, thus the entire digraph is a strong component.")
+        else:
+            sc_c = 1
+            print(f"{self.name} has {strong_components_count} strongly connected components (descendending order, isolated vertices ignored = {exclude_isolated_vertices}):")
+            for strong_component in sorted_strong_components:
+                if exclude_isolated_vertices:
+                    if len(strong_component) == 1:
+                        continue
+                print(f"\t>> strong component #{sc_c}, has vertices: {sorted(strong_component)}"); sc_c += 1
+        print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
 
 class Product_Digraph:
@@ -103,41 +178,53 @@ class Product_Digraph:
         self.D1: Digraph = D1                                    # factor digraph 1
         self.D2: Digraph = D2                                    # factor digraph 2
 
-        self.D1xD2: Digraph = Digraph(nx.tensor_product(self.D1.D, self.D2.D))
+        self.D1xD2: Digraph = Digraph(nx.tensor_product(self.D1.D, self.D2.D),f"{self.D1.name}x{self.D2.name}")
 
-    def get_extremum_k_val_kings(self, extremum_is_max: bool=True):
+    def get_product_extremum_k_val_kings(self, extremum_is_max: bool=True):
         '''
         In the product, will find the k_val kings, either min, or max, based on boolean given in arg.
         :param extremum_is_max: if True, extremum is maximal, otherwise minimum.
         '''
-
-        if self.D1xD2.D_kings == []:
+        print(f"~~~~~~{"MAX" if extremum_is_max else "MIN"} EXTRENUM K_VAL KINGS IN {self.D1xD2.name}~~~~~~")
+        if len(self.D1xD2.D_kings) == 0:
+            print(f"get_product_extrenum_k_val_kings(): {self.D1xD2.name} has no kings, unable to retrieve extrenum k_val kings.")
+            print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
             return
 
-        k_val_list = set()
+        extremum_k_val = self.D1xD2.max_k_val if extremum_is_max else self.D1xD2.min_k_val
 
-        for node in self.D1xD2.D.nodes:
-            if node in self.D1xD2.D_kings:
-                k_val = self.D1xD2.D.nodes[node]['k_val']
-                k_val_list.add(k_val)
-
-
-        extremum_k_val = max(k_val_list) if extremum_is_max else min(k_val_list)
+        print(f"\nget_product_extrenum_k_val_kings(): the following are the {"maximal" if extremum_is_max else "minimal"} k_vals of king vertices in {self.D1xD2.name}...\n")
 
         for node in self.D1xD2.D.nodes:
             if node in self.D1xD2.D_kings and self.D1xD2.D.nodes[node]['k_val'] == extremum_k_val:
                 comp1 = node[0]
                 comp2 = node[1]
 
-                print(f"the vertex {node} in product has {"maximal" if extremum_is_max else "minimal"} k_val {extremum_k_val} in "
-                      f"product digraph, and is composed of {comp1} and {comp2}.")
+                print(f"vertex {node} in {self.D1xD2.name} has {"maximal" if extremum_is_max else "minimal"} k_val {extremum_k_val} in "
+                      f"{self.D1xD2.name}, and is composed of vertex {comp1} of {self.D1.name}, and vertex {comp2} of {self.D2.name}:")
 
-                print(f"vertex {comp1} from D1 has k_val {self.D1.D.nodes[comp1]['k_val']}, and is on closed diwalks of lengt"
+                print(f"\t>> vertex {comp1} from {self.D1.name} has k_val {self.D1.D.nodes[comp1]['k_val']}, and is on closed diwalks of lengt"
                       f"hs (Sv = {self.D1.D.nodes[comp1]['Sv']}), with GCD(Sv) = {self.D1.D.nodes[comp1]['GCD(Sv)']}.")
 
-                print(f"vertex {comp2} from D2 has k_val {self.D2.D.nodes[comp2]['k_val']}, and is on closed diwalks of lengt"
-                      f"hs (Sv = {self.D2.D.nodes[comp2]['Sv']}), with GCD(Sv) = {self.D2.D.nodes[comp2]['GCD(Sv)']}.")
+                print(f"\t>> vertex {comp2} from {self.D2.name} has k_val {self.D2.D.nodes[comp2]['k_val']}, and is on closed diwalks of lengt"
+                      f"hs (Sv = {self.D2.D.nodes[comp2]['Sv']}), with GCD(Sv) = {self.D2.D.nodes[comp2]['GCD(Sv)']}.\n")
 
+        print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
+    def below_upper_bound(self) -> bool:
+        print(f"~~~~~~CHECKING IF {self.D1xD2.name} IS BELOW THEORIZED UPPER-BOUND~~~~~~")
+        upper_bound_val = (self.D1.D.order() * self.D2.D.order()) - 1
+        max_k_val = self.D1xD2.max_k_val
+        if 0 < max_k_val < upper_bound_val:
+            print(f"Maximal k_val of {self.D1xD2.name} is below theorized upper-bound, max_k_val is: {self.D1xD2.max_k_val}; upper_bound_val is: {upper_bound_val}.")
+            print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
+            return True
+        elif max_k_val == upper_bound_val:
+            print(f"Maximal k_val of {self.D1xD2.name} is equal to theorized upper-bound, max_k_val is: {self.D1xD2.max_k_val} = {upper_bound_val}.")
+        else:
+            print(f"{self.D1xD2.name} has no kings, is invalid/unqualified for checking against upper-bound.")
 
-        pass
+        print(f"~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+        return False
+
--- projectFiles/main.py
+++ projectFiles/main.py
@@ -3,28 +3,73 @@ import math
 import functools
 from networkx_viewer import Viewer
 import DKS_tools.Util as util
-from DKS_tools.Analysis import Digraph, Product_DiGraph
+from DKS_tools.Analysis import Digraph, Product_Digraph
 
-T1 = Digraph(util.mackay_t_parser("t_files/tourn5.txt", 3),True)
-T2 = Digraph(util.mackay_t_parser("t_files/tourn5.txt", 3),True)
+"""T1 = Digraph(util.mackay_t_parser(f"t_files/tourn6.txt", 16),f"G",True)"""
 
-print("T1: ")
-print(list(T1.D.nodes(data=True)))
-T1.list_kings()
-
-print("T2: ")
-print(list(T2.D.nodes(data=True)))
-T2.list_kings()
-
-T1xT2 = Product_Digraph(T1, T2)
+# is the length of all closed diwalks containing a specific vertex divisible by the GCD of all lengths of simple_cycles
+# that also contain that vertex?
+G = nx.DiGraph()
+G.add_edges_from([[1,2],[2,3],[3,1],[2,4],[4,6],[6,5],[5,2]])
 
 
-print("T1xT2: ")
 
-print("kings in T1xT2:")
-for k_king in T1xT2.D1xD2.D_kings:
-    print(f"king vertex {k_king}, characteristics: {T1xT2.D1xD2.D.nodes[k_king]}")
+T1 = Digraph(G,"G")
+T2 = Digraph(G,"G")
+print(f"\nEdges of G: {T1.D.edges}\n")
 
-T1xT2.get_extremum_k_val_kings()
+'''
+Below is code for finding the lengths of all closed diwalks containing a king vertex
+'''
+for king in T1.D_kings:
+    T1.D.nodes[king]['Sv'] = set()
 
+kings_to_check = T1.D_kings.copy()
 
+for proposed_walk_length in range(1,T1.D.size()+1):
+    # if there's no more kings to check lengths for, then stop the iteration
+    if len(kings_to_check) == 0:
+        break
+    # find all walks in the digraph of that particular length
+    all_walks_of_len = nx.number_of_walks(T1.D, proposed_walk_length)
+
+    print(f"proposed walk len = {proposed_walk_length}")
+
+    for king in kings_to_check:
+
+        print(f"\tchecking against king vertex {king}")
+        num_king_closed_diwalks = all_walks_of_len[king][king]       # closed diwalks from king to itself of length
+
+        # if any king-to-self closed diwalks are found
+        if num_king_closed_diwalks > 0:
+            # if there exists king-to-self closed diwalks
+            if len(T1.D.nodes[king]['Sv']) > 0:
+                length_is_multiple = False
+                for existing_length in T1.D.nodes[king]['Sv']:
+                    if proposed_walk_length % existing_length == 0:
+                        length_is_multiple = True
+
+                if not length_is_multiple:
+                    print(f"\t\tadding length to Sv")
+                    T1.D.nodes[king]['Sv'].add(proposed_walk_length)
+            else:
+                print(f"\t\tadding length to empty Sv")
+                T1.D.nodes[king]['Sv'].add(proposed_walk_length)
+
+    kings_to_be_removed = list()
+
+    for king in kings_to_check:
+        print(f"\nchecking GCD for vertex {king}")
+        if len(T1.D.nodes[king]['Sv']) >= 2:
+            current_GCD_of_Sv = functools.reduce(math.gcd, T1.D.nodes[king]['Sv'])
+
+            if current_GCD_of_Sv == 1:
+                print(f"\tking {king} flagged for removal")
+                kings_to_be_removed.append(king)
+
+    if len(kings_to_be_removed) != 0:
+        for king in kings_to_be_removed:
+            kings_to_check.remove(king)
+
+for king in T1.D_kings:
+    print(f"Basis Sv of king {king}, is: {T1.D.nodes[king]['Sv']}")
