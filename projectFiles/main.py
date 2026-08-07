c9d055197d1f217fb3cd108eb672a36aea303f64
bee 🐝 <brandonstanton@outlook.com>
2026-08-07 09:12:43 -0700

    separated the experiment configuration integers from the `min_max_k_val_kings_experiment` function call, will be considering moving these to a configuration .txt file, or adding a user prompt to the runtime of the program to have the user put these values in manually

--- projectFiles/main.py
+++ projectFiles/main.py
@@ -9,19 +9,15 @@ from DKS_tools.Experiment_Functions import min_max_k_val_kings_experiment
 
 if __name__ == '__main__':
 
+    # edit these lines to configure experiment
+    specified_order_t = 3
+    specified_line_t = 2
     max_order_t = 5
 
-    # below is a sample line, it will take the second-line tournament from the "tourn3.txt" file contained within the
-    # directory "ProjectFiles\digraph_datasets\t_files\" and take its direct product with all other tournaments up to
-    # order 10, it will take some time, and will output a results file in the "ProjectFiles\experiment results" directory
+    # below is a sample line, it will take the specified-line tournament from the "tourn<specified_order_t>.txt" file
+    # contained within the directory "ProjectFiles\digraph_datasets\t_files\" and take its direct product with all other
+    # tournaments up to 'max_order_t' and will output a results file in the "ProjectFiles\experiment results" directory,
+    # depending on its value, it may take quite some time (if max_order_t == 10, it may take 20+ minutes)
 
-    # Note to user: due to the amount of results, it will initially be split into several "parts"... it will compile all
-    # these result 'parts' into a singular txt document given enough time; these parts result from the parallelization
-    # of completing the task
+    min_max_k_val_kings_experiment(specified_order_t, specified_line_t, max_order_t)
 
-    # the below experiment will result in an approx. 4GB txt file, and took a moderately powered machine about 20 mins
-    # to run to completion... you've been warned!
-
-    min_max_k_val_kings_experiment(3,2, max_order_t)
-
-    pass  # keep this here
